0e72158e94c0f0fa7d6ae9ad2ca6b8fcfea709f7
acamogh <acamogh@gmail.com>
2016-02-19 17:36:06 +0530

    signals

--- products/models.py
+++ products/models.py
@@ -1,6 +1,8 @@
 from __future__ import unicode_literals
 from django.core.urlresolvers import reverse
+
 from django.db import models
+from django.db.models.signals import post_save
 
 
 # Create your models here.
@@ -18,8 +20,6 @@ class ProductManager(models.Manager):
         return self.get_queryset().active()
 
 
-
-
 class Product(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField(blank=True, null=True)
@@ -33,3 +33,38 @@ class Product(models.Model):
 
     def get_absolute_url(self):
         return reverse('Product_Detail', kwargs={'pk': self.pk})
+
+
+class Variation(models.Model):
+    product = models.ForeignKey(Product)
+    title = models.CharField(max_length=120)
+    price = models.DecimalField(decimal_places=2, max_digits=20)
+    sale_price = models.DecimalField(decimal_places=2, max_digits=20, null=True, blank=True)
+    active = models.BooleanField(default=True)
+    inventory = models.IntegerField(null=True, blank=True)  # refer none == unlimited amount
+
+    def __unicode__(self):
+        return self.title
+
+    def get_price(self):
+        if self.sale_price is not None:
+            return self.sale_price
+        else:
+            return self.price
+
+    def get_absolute_url(self):
+        return self.product.get_absolute_url()
+
+
+def product_post_saved_receiver(sender, instance, created, *args, **kwargs):
+    product = instance
+    variations = product.variation_set.all()
+    if variations.count() == 0:
+        new_var = Variation()
+        new_var.product = product
+        new_var.title = "Default"
+        new_var.price = product.price
+        new_var.save()
+
+
+post_save.connect(product_post_saved_receiver, sender=Product)
